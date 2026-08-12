a20e557253055f1f5179597cf992a1d12ab77280
Prathamesh Ganesh Devkar <219124940+prathamesh-499@users.noreply.github.com>
2026-08-13 00:56:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app =express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-	origin: 'https://echoai-97f4.onrender.com/',
+	origin: 'https://echoai-97f4.onrender.com',
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	credentials: true
 }));
